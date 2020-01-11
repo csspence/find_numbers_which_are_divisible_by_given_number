e4c653e8f776470da24c0a5895e14ff8ca0cd196
Crew Spence <csspence123@gmail.com>
2020-01-11 10:43:02 -0600

    Wrote working solution for find_numbers_which_are_divisible_by_given_number.

--- find_numbers_which_are_divisible_by_given_number.js
+++ find_numbers_which_are_divisible_by_given_number.js
@@ -6,6 +6,12 @@ Example
 divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
 */
 
-function divisibleBy(numbers, divisor){
-
+const divisibleBy = (numbers, divisor) => {
+  let arr = [];
+  for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % divisor === 0) {
+      arr.push(numbers[i]);
+    }
+  }
+  return arr;
 }
